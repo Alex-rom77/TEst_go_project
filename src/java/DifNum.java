04354f6dae7111335f4618c1456b6a78e688b567
Skarbaro <47332904+Skarbaro@users.noreply.github.com>
2021-04-13 12:47:59 +0300

    Create DifNum.java

---
+++ src/java/DifNum.java
@@ -0,0 +1,13 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+	
+      int dif, num1 = 10;
+      int num2 = 11;
+
+      dif = num1 + num2;
+
+      System.out.println("Sum of these numbers: " + dif);
+   }
+   
+}
